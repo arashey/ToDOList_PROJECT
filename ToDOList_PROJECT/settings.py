a0da044becb9f6b765aea5208b761ed539d482c5
arashey <arasheypy@gmail.com>
2026-05-30 19:26:11 +0330

    login settings

--- ToDOList_PROJECT/settings.py
+++ ToDOList_PROJECT/settings.py
@@ -126,3 +126,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 DEBUG=True
 
+
+
+
+LOGIN_URL = '/login/'              
+LOGIN_REDIRECT_URL = '/'           
+LOGOUT_REDIRECT_URL = '/login/'    
